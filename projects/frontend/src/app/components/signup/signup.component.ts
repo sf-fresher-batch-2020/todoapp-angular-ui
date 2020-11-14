78617c12e0ba116bb6d7ea4a3186785d9433229f
Pavan Manikanta Kiran Maddu <m.p.m.kiran@gmail.com>
2020-11-14 06:07:22 +0530

    siginup feature added

--- projects/frontend/src/app/components/signup/signup.component.ts
+++ projects/frontend/src/app/components/signup/signup.component.ts
@@ -1,4 +1,9 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { ConfirmedValidator } from '../../confirmed.validator';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+    ) {
+  }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmpassword: ['', [Validators.required]]
+    }, {
+      validator: ConfirmedValidator('password', 'confirmpassword')
+    });
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authService.register(this.form.value).pipe(first()).subscribe(
+      data => {
+        console.log('success', data);
+      },
+      error => {
+        console.log(error);
+      });
   }
 
 }
--- projects/frontend/src/app/services/auth.service.ts
+++ projects/frontend/src/app/services/auth.service.ts
@@ -1,3 +1,6 @@
+import { User } from './../classes/user';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
@@ -7,8 +10,16 @@ import { environment } from '../../environments/environment';
 export class AuthService {
 
   private apiUrl: string;
-  constructor() {
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {
     this.apiUrl = environment.API_URL;
     // console.log(this.apiUrl);
+  }
+
+  register(user: User){
+    const url = this.apiUrl + '/users';
+    return this.http.post(url, user);
   }
 }
